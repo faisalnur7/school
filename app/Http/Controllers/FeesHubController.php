9a7247cf5cd9e46efc9c9ad3654609112cdf9e83
Faisal Nur <md.faisalnur55@gmail.com>
2026-05-13 11:24:42 +0600

    Fee hub updated

--- app/Http/Controllers/FeesHubController.php
+++ app/Http/Controllers/FeesHubController.php
@@ -16,11 +16,11 @@ class FeesHubController extends Controller
                 ['icon' => 'fa-bus',            'title' => 'Transport Fees',     'subtitle' => 'Manage transport fees',       'route' => 'transports.index',           'from' => '#d97706', 'to' => '#b45309'],
             ],
             'Reports' => [
-                ['icon' => 'fa-receipt',        'title' => 'Student Payments',   'subtitle' => 'View payment history',        'route' => 'payments.index',             'from' => '#7c3aed', 'to' => '#6d28d9'],
+                ['icon' => 'fa-chart-pie',      'title' =>'Student Payment Report',     'subtitle' => 'Category-wise payment report','route' => 'fees.payment-report',        'from' => '#0ea5e9', 'to' => '#0369a1'],
+                // ['icon' => 'fa-receipt',        'title' => 'Student Payments',   'subtitle' => 'View payment history',        'route' => 'payments.index',             'from' => '#7c3aed', 'to' => '#6d28d9'],
                 ['icon' => 'fa-file-invoice',   'title' => 'Classwise Due',      'subtitle' => 'Classwise due report',        'route' => 'fees.due-report',            'from' => '#0f766e', 'to' => '#0d9488'],
                 ['icon' => 'fa-user-clock',     'title' => 'Student Due',        'subtitle' => 'Student due report',          'route' => 'fees.student-due-report',    'from' => '#b45309', 'to' => '#92400e'],
                 ['icon' => 'fa-percentage',     'title' => 'Discount List',      'subtitle' => 'View discount list',          'route' => 'fees.discount-list',         'from' => '#be185d', 'to' => '#9d174d'],
-                ['icon' => 'fa-chart-pie',       'title' => 'Payment Report',     'subtitle' => 'Category-wise payment report','route' => 'fees.payment-report',        'from' => '#0ea5e9', 'to' => '#0369a1'],
             ],
         ];
 
